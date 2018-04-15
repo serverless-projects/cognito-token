f53e2790e3b7e1307002a38c62fe2379a80cf199
Bill Wang <ozbillwang@gmail.com>
2018-04-15 12:09:48 +0000

    cognito-token

---
+++ README.md
@@ -0,0 +1,37 @@
+# Tokens with User Pools
+
+This demo shows the real cognito three tokens in the aws document [Using Tokens with User Pools](https://amzn.to/2fo77UI). It also helps you to fully undertand how the payload looks like. 
+
+## Preparation
+
+* create cognito user pool. You can reference the serverless stack in [create_coginto_userpool](https://github.com/serverless-projects/serverless-authorizers/blob/master/cognito/create_coginto_userpool/serverless.yml)
+* create at least one user in cognito user pool. If you want to create new user with command, reference `signup.sh`. You need login aws console to manually confirm this new user.
+* update username, password, user pool id and client id in `config.json`
+
+## Usage
+
+run 
+
+    $ npm install cognito-token
+    $ cognito-token config.json
+    
+    userData: {"Username":"bill","Pool":{"userPoolId":"....
+    ===========================
+    Result methods: idToken,refreshToken,accessToken,clockDrift
+    idToken methods: jwtToken,payload
+    accessToken methods: jwtToken,payload
+    refreshToken methods: token
+    ID token payload's method: sub,email_verified,gender,iss,phone_number_verified,cognito:username,given_name,aud,event_id,token_use,auth_time,phone_number,exp,iat,family_name,email
+    user's mailbox: abc@example.com
+    ===========================
+    ID Token: eyJraWQiOiJoTxxxxVtZm1K5wZmIFA
+    ===========================
+    Access token: eyJraWQiOixxxxy5H5LaCQTw
+    ===========================
+    Refresh token: eyJjdHkiOixxxx8twwVe2CnDKMig
+
+You should get three tokens: id token, access token and refresh token
+
+ID token is the most useful token you need. 
+
+I also added codes to show how to get these three token's methods and how to show the user's attributes, for example, his/her emailbox.
---
+++ token.js
@@ -0,0 +1,84 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const AWS = require("aws-sdk");
+const AmazonCognitoIdentity = require("amazon-cognito-identity-js");
+
+const meow = require('meow');
+const fs = require('fs');
+
+const cli = meow(`
+    Usage
+      # Default configuration file is local ./config.json
+      $ cognito-tool <JSON_config_file>
+`);
+
+module.exports.test = () => {
+
+    const file = cli.input[0] || 'config.json';
+
+    fs.readFile(file, 'utf8', function readFileCallback(err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(data);
+            data = JSON.parse(data);
+
+            var authenticationData = {
+                Username: data.Username,
+                Password: data.Password,
+            };
+            var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
+            var poolData = {
+                UserPoolId: data.UserPoolId,
+                ClientId: data.ClientId
+            };
+            var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+            var userData = {
+                Username: data.Username,
+                Pool: userPool
+            };
+            console.log("userData:", JSON.stringify(userData));
+
+            var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+            cognitoUser.authenticateUser(authenticationDetails, {
+                onSuccess: function(result) {
+                    console.log("===========================");
+                    // Result methods: idToken,refreshToken,accessToken,clockDrift
+                    console.log('Result methods: ' + Object.getOwnPropertyNames(result));
+                    // idToken methods: jwtToken,payload
+                    console.log('idToken methods: ' + Object.getOwnPropertyNames(result.idToken));
+                    // accessToken methods: jwtToken,payload
+                    console.log('accessToken methods: ' + Object.getOwnPropertyNames(result.accessToken));
+                    // refreshToken methods: token
+                    console.log('refreshToken methods: ' + Object.getOwnPropertyNames(result.refreshToken));
+                    // ID token payload's method: sub,email_verified,gender,iss,phone_number_verified,cognito:username,given_name,aud,event_id,token_use,auth_time,phone_number,exp,iat,family_name,email
+                    console.log("ID token payload's method: " + Object.getOwnPropertyNames(result.idToken.payload));
+                    // show the code on how to get cognito user's attribute, for example, its emailbox
+                    console.log("user's mailbox: " + result.idToken.payload.email);
+                    // Amazon Cognito issues three tokens to the client
+                    // https://amzn.to/2fo77UI
+                    /*Use the idToken for Logins Map when Federating User Pools with Cognito Identity or when passing through an Authorization Header to an API Gateway Authorizer*/
+                    console.log("===========================");
+                    console.log('ID token: ' + result.idToken.jwtToken);
+                    //console.log('ID token: ' + result.getIdToken().getJwtToken());
+                    console.log("===========================");
+                    console.log('Access token: ' + result.accessToken.jwtToken);
+                    //console.log('Access token: ' + result.getAccessToken().getJwtToken());
+                    console.log("===========================");
+                    console.log('Refresh token: ' + result.refreshToken.token);
+                    //console.log('Refresh token: ' + result.refreshToken.getToken());
+
+                },
+
+                onFailure: function(err) {
+                    alert(err);
+                },
+
+            });
+        };
+    });
+}
+
+this.test();
